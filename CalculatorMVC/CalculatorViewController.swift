ba18982a392e7b82d645bc8accad445aa702bb7a
Ivan <ivan@macbook-air-ivan.local>
2016-07-25 23:47:10 +0300

    splitViewController modification

--- CalculatorMVC/CalculatorViewController.swift
+++ CalculatorMVC/CalculatorViewController.swift
@@ -9,36 +9,36 @@
 import UIKit
 
 protocol CalculatorBrainDelegate {
-    func trackPending( value: Bool)
+    func trackPending( value: Bool )
 }
 
 class CalculatorViewController: UIViewController, UISplitViewControllerDelegate, CalculatorBrainDelegate {
 
     @IBOutlet private weak var display: UILabel!
-    
     @IBOutlet weak var history: UILabel!
-    
     @IBOutlet weak var graphButton: UIButton!
     
+    private struct Storyboard{
+        static let ShowGraph = "Show Graph"
+    }
+    
     private let decimalSeparator = NSNumberFormatter().decimalSeparator
-    
     private let numberStyle = NSNumberFormatter()
-    
     private var userIsInTheMiddleOfTypingANumber = false
     
     private let defaults = NSUserDefaults.standardUserDefaults()
-    
-    private var settings = [AnyObject]()
-    
+    private struct Keys{
+        static let Program = "CalculatorViewController.Program"
+    }
     typealias PropertyList = AnyObject
-    
-    private var settingsProgram : PropertyList{
+    private var program : PropertyList?{
         get {
-            return settings
+            return defaults.objectForKey(Keys.Program)
+        }
+        set{
+            defaults.setObject(newValue, forKey: Keys.Program)
         }
     }
-    
-    private var isAppLoaded = false
     
     func trackPending(value: Bool) {
         if value {
@@ -74,21 +74,15 @@ class CalculatorViewController: UIViewController, UISplitViewControllerDelegate,
         numberStyle.notANumberSymbol = "Error"
         splitViewController?.delegate = self
         brain.delegate = self
+        if let storedProgram = program {
+            brain.program = storedProgram
+            displayValue = brain.result
+            history.text = brain.description + (brain.isPartialResult ? "..." : "=")
+            if let graphvc = splitViewController?.viewControllers.last?.contentViewController as? GraphViewController {
+                prepareGraph(graphvc)
+            }
+        }
     }
-    
-//    override func viewDidAppear(animated: Bool) {
-//        super.viewDidAppear(animated)
-//        if !isAppLoaded{
-//            if let settingsValues = defaults.objectForKey("calcSettings") as? [AnyObject] where settingsValues.count > 0 {
-//                print("loading setup: \(settingsValues.last!)")
-//                brain.program = settingsValues.last!
-//                displayValue = brain.result
-//                history.text = brain.description + (brain.isPartialResult ? "..." : "=")
-////                performSegueWithIdentifier("Show Graph", sender: nil)
-//            }
-//        }
-//        isAppLoaded = true
-//    }
     
     @IBAction func backSpace(sender: AnyObject) {
         if userIsInTheMiddleOfTypingANumber{
@@ -156,8 +150,7 @@ class CalculatorViewController: UIViewController, UISplitViewControllerDelegate,
         displayValue = nil
         history.text = " "
         userIsInTheMiddleOfTypingANumber = false
-        settings = []
-        defaults.removeObjectForKey("calcSettings")
+        defaults.removeObjectForKey(Keys.Program)
     }
     
     private func prepareGraph(graphvc : GraphViewController){
@@ -169,24 +162,23 @@ class CalculatorViewController: UIViewController, UISplitViewControllerDelegate,
             } else {
                 return 0.0
             }
-            })
-        settings = []
-        settings.append(brain.program)
-        defaults.setObject(settingsProgram, forKey: "calcSettings")
-        print("settings saved: \(settings) program: \(brain.program)")
+        })
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let graphvc = segue.destinationViewController.contentViewController as? GraphViewController{
-            prepareGraph(graphvc)
+        if segue.identifier == Storyboard.ShowGraph {
+            if let graphvc = segue.destinationViewController.contentViewController as? GraphViewController{
+                prepareGraph(graphvc)
+            }
         }
     }
     
     @IBAction func ShowGraph() {
+        program = brain.program
         if let graphvc = splitViewController?.viewControllers.last?.contentViewController as? GraphViewController{
             prepareGraph(graphvc)
         } else {
-            performSegueWithIdentifier("Show Graph", sender: nil)
+            performSegueWithIdentifier(Storyboard.ShowGraph, sender: nil)
         }
     }
     
@@ -201,12 +193,7 @@ class CalculatorViewController: UIViewController, UISplitViewControllerDelegate,
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
         if primaryViewController.contentViewController == self{
             if let graphvc = secondaryViewController.contentViewController as? GraphViewController where graphvc.graphFunc == nil {
-                if let settingsValues = defaults.objectForKey("calcSettings") as? [AnyObject] where settingsValues.count > 0 {
-                    print("loading setup: \(settingsValues.last!)")
-                    brain.program = settingsValues.last!
-                    displayValue = brain.result
-                    history.text = brain.description + (brain.isPartialResult ? "..." : "=")
-                    prepareGraph(graphvc)
+                if program != nil {
                     return false
                 }
                 return true
